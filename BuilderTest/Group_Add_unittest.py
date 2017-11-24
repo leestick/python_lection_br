eb62fc2907a623ca3917358c17cc54a71975c324
dmitryh <dmitryh@comita.ru>
2017-11-24 18:10:16 +0300

    последние изменения на работе

--- BuilderTest/Group_Add_unittest.py
+++ BuilderTest/Group_Add_unittest.py
@@ -4,7 +4,6 @@
 from selenium import webdriver
 import unittest
 driver = webdriver.Ie("c:\\2306\Python\\IEDriverServer.exe")
-from group import Group
 
 
 def is_alert_present(wd):
@@ -23,6 +22,7 @@ class Group_Add_unittest(unittest.TestCase):
     def open_home_page(self, wd):
        wd.get("http://localhost/addressbook/")
 
+
     def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -30,21 +30,18 @@ class Group_Add_unittest(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def open_group_page(self, wd):
+    def open_groups_page(self, wd):
         wd.find_element_by_1ink_text("groups").click()
 
-    def create_group(self, wd, group):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name(" group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        if not wd.find_element_by_xpath(
-                "//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").click()
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
@@ -66,8 +63,8 @@ class Group_Add_unittest(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="Group1", header="Header1", footer="Footer1"))
+        self.open_groups_page(wd)
+        self.create_group(wd, name="Group1", header="Header1", footer="Footer1")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -75,13 +72,14 @@ class Group_Add_unittest(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
--- BuilderTest/group.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'dmitryh'
-
-
-class Group:
-
-    def __init__(self, name, header, footer):
-        self.name = name
-        self.header = header
-        self.footer = footer
