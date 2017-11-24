3ab65558806148ab6a82faddfc040163ebcb5bf2
dmitryh <dmitryh@comita.ru>
2017-11-24 15:46:58 +0300

    Рефакторинг. Вспомогательные методы. Сделали параметризованными Параметры сгруппированы

--- BuilderTest/Group_Add_unittest.py
+++ BuilderTest/Group_Add_unittest.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+"""from selenium.webdriver.firefox.webdriver import WebDriver
+"""
+from selenium import webdriver
+WebDriver = webdriver.Ie("c:\\2306\Python\\IEDriverServer.exe")
+import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -14,36 +17,67 @@ class Group_Add_unittest(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_Group_Add_unittest(self):
-        success = True
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
+
+    def open_home_page(self, wd):
+       wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
+
+    def open_group_page(self, wd):
+        wd.find_element_by_1ink_text("groups").click()
+
+    def create_group(self, wd, group):
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group firm
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Group1")
-        if not wd.find_element_by_xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").is_selected():
+        wd.find_element_by_name(" group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        if not wd.find_element_by_xpath(
+                "//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]").click()
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Header1")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer1")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
+        # return to groups page
         wd.find_element_by_link_text("groups").click()
+
+    def logout(self, wd):
+        # logout
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def test_Group_Add(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group(name="Group1", header="Header1", footer="Footer1"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_Empty_Group_Add(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
